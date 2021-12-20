38af44a490d68075df8c54539dc2d56e6c7c2cf2
21701 <king2doi@gmail.com>
2021-12-20 16:26:40 +0900

    4

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import joblib
 
 from eda_app import run_eda_app
 from ml_app import run_ml_app
+from PIL import Image
+
 
 
 
@@ -17,7 +19,8 @@ def main():
     choice = st.sidebar.selectbox('메뉴',menu)
     
     if choice == '홈' :
-        st.subheader('직원의 정보를 입력하면 퇴사를 예측')
+        st.title('직원의 정보를 입력하면 퇴사를 예측')
+        st.image('https://blog.kakaocdn.net/dn/m14u3/btq0dohqHsv/RgkaxYBBXp7cQsDw1bsug1/img.jpg',use_column_width=True)
 
     elif choice == '데이터분석':
         run_eda_app()
